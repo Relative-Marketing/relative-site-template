af5be3428cf25071c84c0d4889cb04ee8bd1e07e
mrmadhat <danny@websitesbydanny.co.uk>
2019-06-28 17:27:41 +0100

    Test

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -17,3 +17,21 @@ noroot mkdir -p ${VVV_PATH_TO_SITE}/public_html
 noroot touch ${VVV_PATH_TO_SITE}/public_html/index.html
 
 echo "Hello" > ${VVV_PATH_TO_SITE}/public_html/index.html
+
+echo "Copying the sites Nginx config template ( fork this site template to customise the template )"
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
+  echo "Inserting the SSL key locations into the sites Nginx config"
+  VVV_CERT_DIR="/srv/certificates"
+  # On VVV 2.x we don't have a /srv/certificates mount, so switch to /vagrant/certificates
+  codename=$(lsb_release --codename | cut -f2)
+  if [[ $codename == "trusty" ]]; then # VVV 2 uses Ubuntu 14 LTS trusty
+    VVV_CERT_DIR="/vagrant/certificates"
+  fi
+  sed -i "s#{{TLS_CERT}}#ssl_certificate ${VVV_CERT_DIR}/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+  sed -i "s#{{TLS_KEY}}#ssl_certificate_key ${VVV_CERT_DIR}/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
