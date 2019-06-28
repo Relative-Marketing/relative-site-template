da0d3dc6777d1b8a9e805d0371f28a2e4ea54796
mrmadhat <danny@websitesbydanny.co.uk>
2019-06-28 15:21:02 +0100

    Test

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -3,6 +3,6 @@
 
 echo " * TEST TEST TEST"
 
-ssh devrelative@31.193.3.183.srvlist.ukfast.net "pwd" -P 2020
+ssh -vvv devrelative@31.193.3.183.srvlist.ukfast.net "pwd" -P 2020
 
 echo " DID THE PWD"
