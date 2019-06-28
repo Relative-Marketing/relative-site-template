6fe7f69b686f96b8c8f20c9dfc26e892e8e7d331
mrmadhat <danny@websitesbydanny.co.uk>
2019-06-28 15:50:46 +0100

    Test

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -6,3 +6,10 @@ echo " * TEST TEST TEST"
 ssh -vvv devrelative@31.193.3.183.srvlist.ukfast.net "pwd && logout" -P 2020
 
 echo " DID THE PWD"
+
+echo "Setting up the log subfolder for Nginx logs"
+noroot mkdir -p ${VVV_PATH_TO_SITE}/log
+noroot touch ${VVV_PATH_TO_SITE}/log/nginx-error.log
+noroot touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
+
+noroot touch ${VVV_PATH_TO_SITE}/public_html/index.html
