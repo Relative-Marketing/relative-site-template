b3bef9cfa7f0c00b430163767c9ca6579faf3004
mrmadhat <danny@websitesbydanny.co.uk>
2019-07-03 12:36:26 +0100

    fixing excludes on rsync

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -106,7 +106,7 @@ provision_files()
     if [ $EXCLUDES ]; then
         IFS=',' read -ra ADDR <<< "$EXCLUDES"
         for i in "${ADDR[@]}"; do
-            backup_excludes="${backup_excludes}--exclude=\"${WP_PATH}/${i}\" "
+            backup_excludes="${backup_excludes}--exclude=/${WP_PATH}/${i} "
         done
     fi
     
