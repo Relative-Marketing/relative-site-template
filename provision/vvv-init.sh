2454593f509283ef8153713e03d63c64a08180d7
mrmadhat <danny@websitesbydanny.co.uk>
2019-07-04 12:18:10 +0100

    attempting db backup without wp

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -87,8 +87,9 @@ provision_db()
     db_user=`noroot wp config get DB_USER`
     db_pass=`noroot wp config get DB_PASSWORD`
     touch ${VVV_PATH_TO_SITE}/.my.cnf
-    echo -e "[mysqldump]\nuser=${db_user}\npassword=${db_password}" > ${VVV_PATH_TO_SITE}/.my.cnf
-cat ${VVV_PATH_TO_SITE}/.my.cnf
+    echo -e "[mysqldump]\nuser=${db_user}\npassword=${db_pass}" > ${VVV_PATH_TO_SITE}/.my.cnf
+
+    noroot scp -P ${SSH_PORT} ${VVV_PATH_TO_SITE}/.my.cnf ${SSH_USER}@${SSH_HOST}
 #    exec_ssh_cmd "mysqldump -u ${db_user} -p ${db_name} | gzip -9" > dblocal.sql.gz
     
     if [ $? -eq 0 ]; then
